46d86cd199becf8cec07c904ac4b88e2b71e87bd
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2026-01-22 09:55:28 +0100

    fix description

--- src/components/pages/ProjectSection.tsx
+++ src/components/pages/ProjectSection.tsx
@@ -62,7 +62,7 @@ const ProjectsSection = () => {
       src: "/ambassador-figma.png",
       alt: "Image of figma design of the Ambassadors page",
       title: "Figma design",
-      description: "A Figma wireframe for Ambassadors page",
+      description: "A Figma wireframe for Ambassadors page.",
       githubLink: "",
       deployLink:
         "https://www.figma.com/proto/08cDJa5lX23ZBA1B061sKR/UX-Group-assignment?page-id=0%3A1&type=design&node-id=6-4&viewport=770%2C982%2C0.15&t=qhCQQt7FABDMi2gB-1&scaling=scale-down&starting-point-node-id=6%3A4&show-proto-sidebar=1&mode=design",
@@ -72,7 +72,7 @@ const ProjectsSection = () => {
       src: "/ambassador-landing.png",
       alt: "Image of the Ambassadors page",
       title: "OpenAQ Ambassadors",
-      description: "The OpenAQ Ambassadors Program landing page",
+      description: "The OpenAQ Ambassadors Program landing page.",
       githubLink: "https://github.com/openaq/ambassadors.openaq.org",
       deployLink: "https://ambassadors.openaq.org/",
     },
